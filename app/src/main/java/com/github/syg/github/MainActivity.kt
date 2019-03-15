fe458f8408792e836891dfb70550c273cc8e4550
lenove <417173927@qq.com>
2019-03-15 08:48:59 +0800

    Initial commit

---
+++ app/src/main/java/com/github/syg/github/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.github.syg.github
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
